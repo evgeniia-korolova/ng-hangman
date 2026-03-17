335b17ec534c6add8bc31738134942ac99e30eb1
evgeniia-korolova <jkod@ukr.net>
2026-03-17 17:00:01 +0200

    feat-refactor: implement initialization of game stats from localStorage

--- src/app/core/services/game-service.ts
+++ src/app/core/services/game-service.ts
@@ -1,16 +1,17 @@
 import { computed, inject, Injectable, linkedSignal, signal } from '@angular/core';
 import { HangmanService } from './hangman-service';
 import { QuizzItem } from '../models/quizz-item.interface';
-
+import { loadStats } from './storage.helper';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameService {
-  private readonly hangmanService = inject(HangmanService);  
-  readonly gamesNumber = signal(0);
-  readonly wins = signal(0);
-  readonly losses = signal(0);
+  private readonly hangmanService = inject(HangmanService);
+
+  readonly gamesNumber = signal(loadStats().games);
+  readonly wins = signal(loadStats().wins);
+  readonly losses = signal(loadStats().losses);
 
   private readonly quizzItems = this.hangmanService.currentQuizzItems;
 
@@ -23,9 +24,9 @@ export class GameService {
   readonly randomIndex = linkedSignal<number | null>(() => {
     const items = this.quizzItems();
     this.gamesNumber();
-  
+
     if (items.length === 0) return null;
-  
+
     return Math.floor(Math.random() * items.length);
-  });  
+  });
 }
