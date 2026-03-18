95de7a95fe8df1afa0006de4d39fdf56188e8818
evgeniia-korolova <jkod@ukr.net>
2026-03-18 18:32:28 +0200

    feat: display statistics of the current user

--- src/app/features/hangman/actions/actions.ts
+++ src/app/features/hangman/actions/actions.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { HangmanService } from '../../../core/services/hangman-service';
 import { TranslocoPipe } from '@jsverse/transloco';
+import { AuthService } from '../../../core/services/auth-service';
 
 @Component({
   selector: 'app-actions',
@@ -10,6 +11,7 @@ import { TranslocoPipe } from '@jsverse/transloco';
 })
 export class Actions {
   protected readonly hangmanService = inject(HangmanService);
+  protected readonly authService = inject(AuthService);
 
   onCategoryChange(event: Event) {
     const select = event.target as HTMLSelectElement;
