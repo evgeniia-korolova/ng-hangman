a1e8020b93de1e9f6ff28d6f3da5bce1509457a9
evgeniia-korolova <jkod@ukr.net>
2026-03-20 19:49:31 +0200

    feat: add content to footer

---
+++ src/app/shared/custom-select/custom-select.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-select',
+  imports: [],
+  templateUrl: './custom-select.html',
+  styleUrl: './custom-select.scss',
+})
+export class CustomSelect {
+
+}
