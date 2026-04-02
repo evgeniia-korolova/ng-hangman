244461c18534d10dd42dc61e3a82701b6ea46288
evgeniia-korolova <jkod@ukr.net>
2026-04-02 19:19:21 +0300

    feat: add key accessibility and internationalization

--- src/app/shared/auth-actions/auth-actions.ts
+++ src/app/shared/auth-actions/auth-actions.ts
@@ -1,13 +1,15 @@
-import { Component, inject, input, } from '@angular/core';
+import { Component, ElementRef, inject, input, viewChildren, } from '@angular/core';
 import { AuthService } from '../../core/services/auth-service';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-auth-actions',
-  imports: [],
+  imports: [TranslocoPipe],
   templateUrl: './auth-actions.html',
   styleUrl: './auth-actions.scss',
 })
 export class AuthActions {
+  public buttons = viewChildren<ElementRef<HTMLButtonElement>>('authBtn');
   protected readonly authService = inject(AuthService);
 
   readonly buttonsStyle = input.required<string>()
